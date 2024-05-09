8e8009a49a8f89d8f5e789a1ed5df4841161807b
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-09 16:59:56 +0200

    Add movie database connection

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movies;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
    public function index(){
-    return view('guests.welcome');
+
+      $movies = Movies::orderByDesc('id')->get();
+      // dd($movies);
+    return view('guests.welcome', compact('movies'));
    }
 }
